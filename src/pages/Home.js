603e092dfd6385bc754693283dc550a8295c29eb
HyeongMinLim <hymin1119@gmail.com>
2022-10-28 17:36:08 +0900

    practice

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// function Home () {
+//     return <h1>Home 화면 입니다.</h1>
+// }
+
+// export default Home;
+
+const Home = () => {
+    return <h1>Home 화면 입니다.</h1>
+}
+
+export default Home;
